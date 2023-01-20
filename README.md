1b39fb777ca5c1f4ca1e0ba556bf707e9729fa9e
System Administrator <root@haiders-MacBook-Air.local>
2023-01-20 09:01:53 +0000

    init

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# Credit-Card-Processing
-Full stack application for a credit card provider. It allows a user to add new credit card and view them in a list form in reactjs. And the backend is in Nodejs with redis.
+Credit Card Processing.
+
+Below you will find some information on how to perform common tasks.<br>
+
+## Table of Contents
+- [Folder Structure](#folder-structure)
+- [Available Scripts](#available-scripts)
+  - [npm start](#npm-start)
+  - [npm test](#npm-test)
+  - [npm run build](#npm-run-build)
+  - [npm run eject](#npm-run-eject)
+
+
+## Folder Structure
+After creation, your project should look like this:
+
+```
+my-app/
+  README.md
+  node_modules/
+  package.json
+  public/
+    index.html
+    favicon.ico
+  src/
+    App.css
+    App.js
+    App.test.js
+    index.css
+    index.js
+    logo.svg
+```
+
+For the project to build, **these files must exist with exact filenames**:
+
+* `public/index.html` is the page template;
+* `src/index.js` is the JavaScript entry point.
+
+## Available Scripts
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.<br>
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.<br>
+
+### `npm test`
+
+Launches the test runner in the interactive watch mode.<br>
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.<br>
---
+++ src/App.js
@@ -0,0 +1,181 @@
+import React, { Component } from 'react';
+import axios from "axios";
+import './App.css';
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            card: '',
+            limit: '',
+            data: [],
+            response: '',
+            messageValid: true,
+            errorName: true,
+            cardError: true,
+            limitError: true,
+            formValid: false
+        }
+        this.apiUrl = 'http://localhost:3001/api/'
+        this.validCreditCard = this.validCreditCard.bind(this);
+        this.validateName = this.validateName.bind(this);
+        this.validateLimit = this.validateLimit.bind(this);
+        this.updateState = this.updateState.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.checkFormValidation = this.checkFormValidation.bind(this)
+        this.getCardsList = this.getCardsList.bind(this)
+        this.addCard = this.addCard.bind(this)
+        this.getCardsList()
+    };
+    /* Get the credit cards list */
+    getCardsList() {
+        try {
+            axios.get(this.apiUrl+'cards_list')
+                .then(data => this.setState({data: data.data.data}))
+                .catch(error => console.log(error)) 
+        } catch (error) {
+            console.log('error', error)
+        }
+    }
+    /* Add the credit card */
+    addCard(data, e) {
+        try {
+            axios.post(this.apiUrl+'add_card', data)
+                .then(data => {
+                    this.setState({response: data.data})
+                    this.showMessage(data.data.message, e)
+                })
+                .catch(error => console.log(error)) 
+        } catch (error) {
+            console.log('error', error)
+        }
+    }
+    /* Credit card validation with luhn Algorithm */
+    validCreditCard(value) {
+          if (/^[0-9]{13,19}$/.test(value)) {
+            /* Luhn Algorithm */
+            let nCheck = 0, bEven = false;
+            value = value.replace(/\D/g, "");
+            for (var n = value.length - 1; n >= 0; n--) {
+                var cDigit = value.charAt(n),
+                        nDigit = parseInt(cDigit, 10);
+                if (bEven && (nDigit *= 2) > 9) nDigit -= 9;
+                nCheck += nDigit;
+                bEven = !bEven;
+            }
+            return (nCheck % 10) == 0;
+        } else {
+            return false
+        }
+      }
+    /* Name string validation */
+    validateName(input) {
+        if(input.length >= 2) {
+            return true;
+        } return false;
+    }
+    /* Limit validation */
+    validateLimit(input) {
+        if(input < 1) return false
+        var regexp = /^[0-9]{1,8}$/;
+        return regexp.test(input);
+    }
+    /* Show APIs reponse messages */
+    showMessage(message, e) {
+        if(message) {
+            this.setState({messageValid: message});
+            if(this.state.response["statusCode"] === 200) {
+                document.getElementById('message').style.color = "green";
+            } else {
+                document.getElementById('message').style.color = "red";
+            }
+        } else {
+            this.setState({messageValid: ''});
+        }
+    }
+
+    /* Updates the state by taking in an event, a validator function (i.e validateName, validateLimit), 
+    and an error name to update the state of. Note: class name and state name are the same*/
+    updateState(e, validatorFunction, errorName) {
+            this.showMessage('', e)
+            setTimeout(this.checkFormValidation, 100);
+            if (validatorFunction === true) {
+                this.setState({[e.target.classList[0]]: e.target.value});
+                this.setState({[errorName]: 'Valid'});
+                document.getElementById(e.target.classList[0]).style.color = "green";
+            }
+            else if (e.target.value.length === 0) {
+                this.setState({[errorName]: 'Empty submission'});
+                document.getElementById(e.target.classList[0]).style.color = "red";
+            }
+            else {
+                this.setState({[errorName]: 'Invalid form input'});
+                document.getElementById(e.target.classList[0]).style.color = "red";
+            }
+    }
+    /* Handling the add credit card form submission */
+    handleSubmit(e) {
+        if(this.state.formValid) {
+        let body = {name: this.state.name, cardNumber: this.state.card, limit: this.state.limit}
+        this.addCard(body, e)
+        this.getCardsList()
+        }
+    }
+    /* Form validation */
+    checkFormValidation(){
+        this.setState({formValid: false});
+        if(this.state.cardError == 'Valid' && this.state.nameError == 'Valid' && this.state.limitError == 'Valid' ) {
+            this.setState({formValid: true});
+        }
+    }
+
+
+  render() {
+    return (
+      <div className="App">
+        <h4>Credit Card System</h4>
+        <h6>Add</h6>
+        {/* Add credit card form */}
+          <form onSubmit={this.handleSubmit}>
+               Name <br/>
+              <input className="name" type="text"id="input-text-sm"
+                     onChange={(event) => this.updateState(event, this.validateName(event.target.value), "nameError")}/>
+              <p id="name">  {this.state.nameError} </p> <br/>
+              Card number <br/>
+              <input className="card" type="text"
+                     onChange={(event) => this.updateState(event, this.validCreditCard(event.target.value), "cardError")}/>
+              <p id="card">  {this.state.cardError} </p> <br/>
+              Limit <br/>
+              <input className="limit" type="text"
+                     onChange={(event) => this.updateState(event, this.validateLimit(event.target.value), "limitError")}/>
+              <p id="limit">  {this.state.limitError} </p> <br/>
+              <button type = 'submit' disabled={!this.state.formValid} className='btn'>Add</button><br/><br/>
+              <p id='message'>{this.state.messageValid}</p>
+          </form>
+          <br/> <br/>
+          {/* Credit cards list */}
+          <h6>Existing Cards</h6>
+          <table>
+            <tr>
+            <th>Name</th>
+            <th>Card Number</th>
+            <th>Balance</th>
+            <th>Limit</th>
+            </tr>
+            {this.state.data.map((val, key) => {
+            return (
+                <tr key={key}>
+                <td>{val.name}</td>
+                <td>{val.cardNumber}</td>
+                <td style={{ color: val.balance < 0 ? "red" : "#5d5d5d" }}><span>&#163;</span> {val.balance}</td>
+                <td><span>&#163;</span>{val.limit}</td>
+                </tr>
+            )
+            })}
+          </table>
+      </div>
+    );
+  }
+}
+
+export default App;
